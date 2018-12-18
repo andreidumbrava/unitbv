55c954dc59a5a51794924bc2d3d7cfcd9a9de2aa
Andrei Dumbravă <19547269+andreidumbrava@users.noreply.github.com>
2018-12-18 18:01:30 +0200

    Session revived from cookie

--- auth_me.php
+++ auth_me.php
@@ -11,15 +11,38 @@
 
    if ($username == "andrei" && $password == "andrei"){
        session_start();
+       setcookie("my_cookie", "my_cookie", time()+3600);
    }
    if (session_status() != PHP_SESSION_ACTIVE){
         echo "<h1>No session started yet</h1>";
-    } else {
+
+        if (isset($_COOKIE['my_cookie'])){
+            echo "A valid cookie detected<br/>";
+            session_start();
+
+        }
+
+    } 
+    
+    if (session_status() != PHP_SESSION_ACTIVE){
+        echo "<h1>No session started yet</h1>";
+    }
+    else {
         echo "<h1>A session was detected with status ";
         echo session_status();
         echo " and id ".session_id();
         echo "</h1>";
     }
+
+    echo "Session: <br />";
+        foreach ($_SESSION as $key=>$value){
+            echo $key."=".$value."<br/>";
+        }
+
+        echo "Cookies: <br/>";
+        foreach ($_COOKIE as $key=>$value){
+            echo $key."=".$value."<br/>";
+        }
 ?>
 </body>
 </html>
