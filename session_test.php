115f6d969c396cc4adc2f1252e0fd5e177c37e3a
Andrei Dumbravă <19547269+andreidumbrava@users.noreply.github.com>
2018-12-18 17:16:08 +0200

    Second version of file
    Just testing a simple php session

---
+++ session_test.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang=" en">
+<head>
+    <meta charset="utf-8">
+    <title>No_Session_PHP_Test</title>
+</head>
+<body>
+<?php
+    if (session_status() == PHP_SESSION_NONE){
+        echo "<h1>No session started yet</h1>";
+
+        session_start();
+
+        if (session_status() != PHP_SESSION_NONE){
+            echo "<br /> New Session Started: ";
+            echo session_id();
+            echo "<pre>";
+            foreach ($_SESSION as $key =>$val) echo $key." ".$val."<br />";
+            echo "</pre>";
+        }
+
+    } else {
+        echo "<h1>A session was detected with status ";
+        echo session_status();
+        echo "</h1>";
+    }
+?>
+</body>
+</html>
+
+
